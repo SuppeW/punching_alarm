bd0ae5deb3b521d1917477c4974f7e078c00217b
SuppeW <anders.woien96@gmail.com>
2018-05-23 18:07:11 +0200

    litt endringer til shock_alarm.py

--- shock_alarm.py
+++ shock_alarm.py
@@ -3,7 +3,7 @@ import time
 import random
 import RPi.GPIO as GPIO
 
-shock_pin = 5
+shock_pin = 17
 sounds = ["chickencoop.mp3","where is my team.mp3","whaaat.mp3"]
 soundslen = len(sounds)
 from random import randint
@@ -12,26 +12,20 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(shock_pin, GPIO.IN)
 
 pygame.mixer.init()
-    def callback(shock_pin):
+def callback(shock_pin):
         if GPIO.input(shock_pin):
             print "alarm time"
             pygame.mixer.music.load(sounds[randint(0,soundslen-1)])
             pygame.mixer.music.play()
-            while pygame.mixer.music.get_busy() == true:
+            while pygame.mixer.music.get_busy() == True:
                 continue
 
 
         else:
             print "alarm time"
-            pygame.mixer.music.load(sounds[sounds[randint(0,soundslen-1)])
-            pygame.mixer.music.play()
-            while pygame.mixer.music.get_busy() == true:
-                continue
 
-
-
-GPIO.add_event_detect(shock_pin, GPIO.BOTH, bouncetime=300)
+GPIO.add_event_detect(shock_pin, GPIO.BOTH, bouncetime=5000)
 GPIO.add_event_callback(shock_pin, callback)
 
 while True:
-    time.sleep(1)
+    time.sleep(5000)
